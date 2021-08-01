988ecb776b5566ecf05f3ac95c21d418e0599eb6
dead-jaskier <symon3@protonmail.com>
2021-08-01 12:20:41 -0500

    Add project files.

---
+++ ozsec2021_shellcode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ozsec2021_shellcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
